cf7477586d3f62e1aab95330ecc6d6fcac0a92b7
khang.chau.26102017@hcmut.edu.vn <khang.chau.26102017@hcmut.edu.vn>
2024-04-23 15:45:21 +0700

    first step

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-# from fastapi import FastAPI
-#
-# app = FastAPI()
-# @app.get("/")
-# async def read_root():
-#     return {"message": "Hello World!"}
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World!"}
