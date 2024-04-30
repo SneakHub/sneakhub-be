df356c107ef33cc166454cb5eedea7764ec8d432
khang.chau.26102017@hcmut.edu.vn <khang.chau.26102017@hcmut.edu.vn>
2024-04-30 15:22:50 +0700

    implement crud methods

--- app/routers/items/services.py
+++ app/routers/items/services.py
@@ -13,3 +13,34 @@ def find_one(id: str) -> Union[Item, None]:
     if not item:
         raise Exception(f"no item with value={id} is found!")
     return item
+
+def create_one(data: BaseItem) -> Item:
+    global items
+    item = Item(**data.dict())
+
+    # add new item into the database
+    items = [*items, item]
+    return item
+
+def delete_one(id: str) -> Item:
+    global items
+    item = find(lambda item: item.id == id, items)
+    if not item:
+        raise Exception(f"no item with value={id} is found!")
+
+    # delete the found item
+    items = list(filter(lambda item: item.id != id, items))
+    return item
+
+
+def update_one(id: str, data: Item) -> Item:
+    global items
+    item = find(lambda item: item.id == id, items)
+    if not item:
+        raise Exception(f"no item with value={id} is found!")
+
+    # delete the found item
+    items = list(map(lambda item: data if item.id == id else item, items))
+    return item
+
+
